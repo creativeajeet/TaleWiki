21d4a74b766ac14744915fd46dd76335fab1e65a
creativeajeet <creative.ajeet87@gmail.com>
2013-06-24 12:23:49 +0530

    My first Ruby Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaleWiki::Application.config.secret_token = '52c6e8036a1434e1494fdbfab8815ca3f6a11a9990284487788113bf2f0f981cfa34f38ddfab578226d96c795254de160299be530feba3134bc226d5d2cb6c0b'
